16d9f740dc5e15cb717166d79e5b2ddd4f25f47a
braxley <58529250+braxley@users.noreply.github.com>
2021-10-18 20:49:34 +0200

    wrote tests for timeConversionPipe

--- src/app/shared/pipes/time-conversion.pipe.spec.ts
+++ src/app/shared/pipes/time-conversion.pipe.spec.ts
@@ -1,8 +1,19 @@
 import { TimeConversionPipe } from './time-conversion.pipe';
 
 describe('TimeConversionPipe', () => {
+  const pipe = new TimeConversionPipe();
   it('create an instance', () => {
-    const pipe = new TimeConversionPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should correctly convert into hours', () => {
+    expect(pipe.transform(60)).toBe('1 hour');
+    expect(pipe.transform(148)).toBe('2 hours 28 minutes');
+    expect(pipe.transform(208)).toBe('3 hours 28 minutes');
+  });
+
+  it('should not convert amounts less than an hour', () => {
+    expect(pipe.transform(1)).toBe('1 minute');
+    expect(pipe.transform(59)).toBe('59 minutes');
   });
 });
--- src/app/shared/pipes/time-conversion.pipe.ts
+++ src/app/shared/pipes/time-conversion.pipe.ts
@@ -10,8 +10,18 @@ export class TimeConversionPipe implements PipeTransform {
       hours++;
       minutes -= 60;
     }
+    let hoursString = '';
+    let minutesString = '';
+    if (hours > 0) {
+      hoursString = hours + ' hour';
+      hoursString += hours > 1 ? 's' : '';
+    }
+    if (minutes > 0) {
+      minutesString = minutes + ' minute';
+      minutesString += minutes > 1 ? 's' : '';
+    }
     return hours > 0
-      ? hours + ' hours ' + minutes + ' minutes'
-      : minutes + ' minutes';
+      ? (hoursString + ' ' + minutesString).trim()
+      : minutesString;
   }
 }
