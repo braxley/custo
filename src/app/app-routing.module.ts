e9455124f613dbf8b8b3a4b9c8264d40eb961f7c
braxley <58529250+braxley@users.noreply.github.com>
2021-10-21 16:55:38 +0200

    added authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { MediumSearchComponent } from './features/medium-search/medium-search.component';
 import { MyMoviesComponent } from './features/my-movies/my-movies.component';
 
 const routes: Routes = [
   { path: '', component: MediumSearchComponent },
+  { path: 'search', redirectTo: '' },
   { path: 'login', component: AuthComponent },
-  { path: 'my-movies', component: MyMoviesComponent },
+  { path: 'my-movies', component: MyMoviesComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
