3021a9cd53e6878bb4c3e38e6e2ba95c59ffb3e3
braxley <58529250+braxley@users.noreply.github.com>
2021-10-28 13:43:58 +0200

    added form to OurMovies

--- src/app/features/our-movies/our-movies.component.ts
+++ src/app/features/our-movies/our-movies.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './our-movies.component.html',
-  styleUrls: ['./our-movies.component.scss']
+  styleUrls: ['./our-movies.component.scss'],
 })
 export class OurMoviesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  onSubmit(form: NgForm) {}
 }
--- src/app/features/our-movies/our-movies.module.ts
+++ src/app/features/our-movies/our-movies.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OurMoviesComponent } from './our-movies/our-movies.component';
-
-
+import { OurMoviesComponent } from './our-movies.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    OurMoviesComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [OurMoviesComponent],
+  imports: [CommonModule, FormsModule],
 })
-export class OurMoviesModule { }
+export class OurMoviesModule {}
