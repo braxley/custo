11b70d36aa07e17e1d0ed6fb6a3753862d06508e
braxley <58529250+braxley@users.noreply.github.com>
2021-11-12 20:32:25 +0100

    changed myMovies detection strategy

--- src/app/features/my-movies/my-movies.component.ts
+++ src/app/features/my-movies/my-movies.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UserMoviesService } from 'src/app/core/services/user-movies.service';
 
 @Component({
   templateUrl: './my-movies.component.html',
   styleUrls: ['./my-movies.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MyMoviesComponent {
   isLoading$ = this.userMoviesService.isLoading$;
